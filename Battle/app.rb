bc32387f1a983bef1ec6d07838377a56241945ab
Dhara <dhara.patel95@hotmail.co.uk>
2020-06-22 15:54:55 +0100

    Added server routes

---
+++ Battle/app.rb
@@ -0,0 +1,20 @@
+require 'sinatra'
+
+enable :sessions
+      set :session_secret, "secret"
+
+get '/' do
+  "Hello World"
+end
+
+get '/secret' do
+  "Hello!"
+end
+
+get '/secret2' do
+  "Hello 2!"
+end
+
+get '/secret3' do
+  "Hello 3!"
+end
