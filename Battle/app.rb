7d70ea0c3cb9b4aad004aaee446318568efc882d
stupot1 <scpitc@gmail.com>
2020-06-22 15:34:14 +0000

    Returning HTML - Working Cat Image return with border in HTML and CSS examples

--- Battle/app.rb
+++ Battle/app.rb
@@ -6,7 +6,11 @@ enable :sessions
 get '/' do
   "Hello World"
 end
-
+#   <div>'http://bit.ly/1eze8aE'</div>  
+#   div {
+#   
+# }
+# <h3>CAT!</h3>
 get '/secret' do
   "Hello!"
 end
@@ -18,3 +22,27 @@ end
 get '/secret3' do
   "Hello 3!"
 end
+
+get '/cat' do
+  erb :cat
+end
+__END__
+@@ cat
+<head>
+  <style>
+  .borderexample {
+   border-style:dashed;
+   border-color:red;
+  }
+  </style>
+</head>
+
+<p style="border:3px; border-style:dashed; border-color:#FF0000; padding: 1em;">
+  <img src=http://bit.ly/1eze8aE alt="Cat">
+</p>
+
+<p class="borderexample">
+  <img src=http://bit.ly/1eze8aE alt="Cat">
+</p>
+
+
