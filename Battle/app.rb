61bdd5e0deb5b4eaddea85911f1c531c332e7a26
stupot1 <scpitc@gmail.com>
2020-06-22 15:42:24 +0000

    Views - Moved HTML and CSS code from app.rb to views/index.erb and modified app.rb to point to index.rb for /cat

--- Battle/app.rb
+++ Battle/app.rb
@@ -1,16 +1,12 @@
 require 'sinatra'
 
 enable :sessions
-      set :session_secret, "secret"
+set :session_secret, "secret"
 
 get '/' do
   "Hello World"
 end
-#   <div>'http://bit.ly/1eze8aE'</div>  
-#   div {
-#   
-# }
-# <h3>CAT!</h3>
+
 get '/secret' do
   "Hello!"
 end
@@ -24,25 +20,5 @@ get '/secret3' do
 end
 
 get '/cat' do
-  erb :cat
+  erb (:index)
 end
-__END__
-@@ cat
-<head>
-  <style>
-  .borderexample {
-   border-style:dashed;
-   border-color:red;
-  }
-  </style>
-</head>
-
-<p style="border:3px; border-style:dashed; border-color:#FF0000; padding: 1em;">
-  <img src=http://bit.ly/1eze8aE alt="Cat">
-</p>
-
-<p class="borderexample">
-  <img src=http://bit.ly/1eze8aE alt="Cat">
-</p>
-
-
