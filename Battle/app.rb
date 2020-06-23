b87bf61c37a929582957d38f77b46f26fd2347e4
stupot1 <scpitc@gmail.com>
2020-06-23 14:15:24 +0000

    Added form and then moved form to nameform.erb and rendered named-cat through post request from form

--- Battle/app.rb
+++ Battle/app.rb
@@ -24,7 +24,12 @@ get '/random-cat' do
   erb (:index)
 end
 
-get '/named-cat' do
+post '/named-cat' do
+  p params
   @name = params[:name]
   erb (:index)
+end
+
+get '/name-form' do
+  erb (:nameform)
 end
